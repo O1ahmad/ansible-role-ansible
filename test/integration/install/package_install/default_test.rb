9a1015317003e7b970a8df6fca6eeed21546b584
Ahmad Wilson <ahwilson712@gmail.com>
2019-12-19 21:46:29 -0500

    add initial templating

---
+++ test/integration/install/package_install/default_test.rb
@@ -0,0 +1,14 @@
+title "Lotus git installation test suite"
+
+describe file('/opt/lotus') do
+  it { should exist }
+end
+
+describe file('/usr/local/bin/lotus') do
+  it { should exist }
+  its('mode') { should cmp '00755' }
+end
+
+describe command('/usr/local/bin/lotus --help') do
+  its('exit_status') { should eq 0 }
+end
